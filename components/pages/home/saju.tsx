c22657ea0e996f3dd1a56288238d4f199d227422
Shayrol <rus47@naver.com>
2025-08-29 18:18:29 +0900

    perf: 이미지 최적화

--- components/pages/home/saju.tsx
+++ components/pages/home/saju.tsx
@@ -1,13 +1,19 @@
 import SajuTable from "./saju_table";
+import Image from "next/image";
 
 export default function Saju({ name }: { name: string }) {
   return (
     <div className="w-[93.6%] max-w-[420px] aspect-[351/621] relative">
       {/* 고정 비율 컨테이너 */}
-      <div className="
-        absolute inset-0 flex flex-col p-[2%] py-[3%] pt-[12%] gap-[1.5%] bg-[url('/saju/saju.svg')]
-        bg-no-repeat bg-contain bg-center
-      ">  
+      <Image
+        src="/saju/saju.svg"
+        alt="사주 배경"
+        layout="fill" // absolute + 부모에 맞춤
+        className="object-contain" // contain으로 비율 유지
+        priority // preload (중요 콘텐츠)
+      />
+
+      <div className="absolute inset-0 flex flex-col p-[2%] py-[3%] pt-[12%] gap-[1.5%]">
         <header className="flex flex-col justify-center items-center gap-[0.8%] w-full">
           <h2 className="text-[min(4vw,18px)] text-center text-[#424242] font-normal whitespace-nowrap">
             {name}님의 사주
